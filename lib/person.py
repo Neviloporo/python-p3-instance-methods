523c1a0d072927c79fcb1da36b65f93826e8c49f
nevil <nevil.oporo@student.moringaschool.com>
2025-05-10 13:40:42 +0300

    instance-methods

--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,16 @@
 
 class Person:
     # Class body goes here
+    def __init__(self, name="Unnamed", age=0):
+        self.name = name
+        self.age = age
 
     #Instance method definition
-    pass
+    def say_hello(self):
+        print(f"Hello, my name is {self.name}.")
+
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
